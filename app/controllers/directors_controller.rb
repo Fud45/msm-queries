c651bf5e2e264b2c9d174ef524e06673ff4b7715
Fud45 <165224436+Fud45@users.noreply.github.com>
2024-05-21 23:22:10 +0000

    movie assignment #1

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,13 +1,27 @@
-class DirectorsController < ApplicationController 
+class DirectorsController < ApplicationController
   def index
-    render ({:template => "director_templates/list"})
+    render ({ :template => "director_templates/list" })
   end
 
   def show
     the_id = params.fetch("the_id")
-   matching_records = Director.where({ :id => the_id })
-   @the_director =matching_records.at(0)
-   
-    render ({:template => "director_templates/details"})
+    matching_records = Director.where({ :id => the_id })
+    @the_director = matching_records.at(0)
+
+    render ({ :template => "director_templates/details" })
+  end
+
+  def senior
+    matching_records = Director.where.not({ :dob => nil }).order({ :dob => :asc }).limit(1)
+    @the_director = matching_records.at(0)
+
+    render ({ :template => "director_templates/eldest" })
+  end
+
+  def junior
+    matching_records = Director.where.not({ :dob => nil }).order({ :dob => :desc }).limit(1)
+    @the_director = matching_records.at(0)
+
+    render ({ :template => "director_templates/youngest" })
   end
 end
